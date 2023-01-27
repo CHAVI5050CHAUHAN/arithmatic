66e88d4f7fa8e0357b369cf2cea8a027c48f877a
CHAI5050CHAUHAN <chavi5050chauhan@gmail.com>
2023-01-27 09:40:28 +0530

    My Second Java

--- src/Arithmatic.java
+++ src/Arithmatic.java
@@ -1,3 +1,5 @@
+
+
 public class Arithmatic {
     public int add(int a ,int b){
         return a+b;
@@ -6,16 +8,17 @@ public class Arithmatic {
         return a-b;
     }
     public int mul(int a ,int b){
-        return a*b;
+        return a*b*a;
     }
     public int div(int a ,int b){
         return a/b;
     }
+
     public static void main(String[] args) {
         Arithmatic arthicmethic = new Arithmatic();
         System.out.println("add-" +arthicmethic.add(2,1));
         System.out.println("sub-" +arthicmethic.sub(2,1));
-        System.out.println("mul-" +arthicmethic.mul(2,1));
+        System.out.println("mul-" +arthicmethic.mul(2,10));
         System.out.println("div-" +arthicmethic.div(2,1));
     }
 }
